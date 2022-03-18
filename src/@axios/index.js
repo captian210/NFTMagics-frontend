4fec38de6207cedc7d85ec9e49e414383c3e12d0
captian210 <captian0210@gmail.com>
2022-03-18 23:16:17 +1000

    initial commit

--- src/@axios/index.js
+++ src/@axios/index.js
@@ -2,7 +2,8 @@ import Axios from "axios";
 
 const axios = Axios.create({
     responseType: "json",
-    baseURL: 'http://localhost:5000',
+    // baseURL: 'http://localhost:5000',
+    baseURL: 'https://nftmagic.herokuapp.com/',
     headers: {
         'Content-Type': 'application/json',
         'accept': 'application/json',
