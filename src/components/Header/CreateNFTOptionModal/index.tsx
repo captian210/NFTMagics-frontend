4ecaa3d9b4f46068fac9601c67483e3596b3bb04
captian210 <captian0210@gmail.com>
2022-03-22 18:57:56 +1000

    initial commit

--- src/components/Header/CreateNFTOptionModal/index.tsx
+++ src/components/Header/CreateNFTOptionModal/index.tsx
@@ -1,19 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
-import { CreateNFTModal } from './styles';
+import { Section, CreateNFTModal } from './styles';
 import Link from 'next/link';
 
 export default function CreateNFTOptionModal() {
     const [open, setOpen] = React.useState(false);
     const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const fullScreen = useMediaQuery(theme.breakpoints.down('lg'));
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -24,28 +20,36 @@ export default function CreateNFTOptionModal() {
     };
 
     return (
-        <div>
+        <Section>
             <button className='button button-primary' onClick={handleClickOpen}>
                 Create NFT
             </button>
             <Dialog
+                maxWidth={'lg'}
                 fullScreen={fullScreen}
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="create-nft-modal-title"
+                className='create-modal'
             >
                 <CreateNFTModal>
-                    <DialogContent className=''>
-                        <DialogTitle id="create-nft-modal-title" className='modal-title'>
+                    <div className="modal-body">
+                        <div id="create-nft-modal-title" className='modal-title'>
                             {"Change the World with your design"}
-                        </DialogTitle>
-                        <div className="modal-body">
+                        </div>
+                        <div className='modal-content'>
                             <Link href={'/createself'}>
                                 <a href="" className='select-item'>
                                     <img src="/images/design/self-logo.png" alt="" />
                                     <button>YOUR SELF</button>
                                 </a>
                             </Link>
+                            <div className='separater'>
+                                <div className='vertical-line'></div>
+                                <div className='mark'>
+                                    <div>OR</div>
+                                </div>
+                            </div>
                             <Link href={'/create'}>
                                 <a className='select-item'>
                                     <img src="/images/design/desingn-logo.png" alt="" />
@@ -53,14 +57,14 @@ export default function CreateNFTOptionModal() {
                                 </a>
                             </Link>
                         </div>
-                    </DialogContent>
-                    <DialogActions className='modal-actions'>
-                        <Button autoFocus onClick={handleClose}>
-                            Cancel
-                        </Button>
-                    </DialogActions>
+                        <div className='modal-actions'>
+                            <Button autoFocus onClick={handleClose}>
+                                Cancel
+                            </Button>
+                        </div>
+                    </div>
                 </CreateNFTModal>
             </Dialog>
-        </div>
+        </Section>
     );
 }
--- src/components/Header/CreateNFTOptionModal/styles.ts
+++ src/components/Header/CreateNFTOptionModal/styles.ts
@@ -1,118 +1,170 @@
 import { styled } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
+export const Section = styled('div')((theme) => {
+  return ({
+  })
+})
 export const CreateNFTModal = styled('div')((theme) => {
 
   const sm = useMediaQuery('(max-width:600px)');
   const md = useMediaQuery('(max-width:900px)');
 
   return ({
-    // backgroundImage: 'linear-gradient(rgb(255 255 255 / 9%), rgb(137 137 137 / 99%))',
-    
-    // '& .diaplog-border': {
-    //   position:'absolute',
-    //   background:'red',
-    //   width: '100%',
-    //   height: '100%',
-    //   animation: 'o-rotate-360 linear 8s infinite',
-    //   '& span': {
-    //     display: 'block',
-    //     width: '100%',
-    //     height: '100%',
-    //     position: 'relative',
-    //     transform: 'translate(-50%, -50%)',
-    //     '&:after': {
-    //       display: 'block',
-    //       content: '""',
-    //       width: '100%',
-    //       height: '100%',
-    //       position: 'absolute',
-    //       left: '100%',
-    //     },
-    //     '&:first-of-type': {
-    //       background: '#e85f99',
-    //       '&:after': {
-    //         background: '#50bda1'
-    //       }
-    //     },
-    //     '&:last-of-type': {
-    //       background: '#f18867',
-    //       '&:after': {
-    //         background: '#65587f'
-    //       }
-    //     }
-    //   },
-    //   '@keyframes o-rotate-360': {
-    //     '0%': {
-    //       transform: 'rotate(0)'
-    //     },
-    //     '100%': {
-    //       transform: 'rotate(360deg)'
-    //     }
-    //   }
-    // },
-    '& .modal-title': {
-      fontSize: '1.7rem',
-      fontWeight: 600,
-      textAlign: 'center'
+    position: 'relative',
+    ...(sm && {
+      overflow: 'auto'
+    }),
+    '& .modal-border': {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      animation: 'o-rotate-360 linear 12s infinite',
+      ...(sm && {
+        display: 'none'
+      }),
+      '& span': {
+        display: 'block',
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        transform: 'translate(-50%, -50%)',
+        '&:after': {
+          display: 'block',
+          content: '""',
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          left: '100%',
+        },
+        '&:first-of-type': {
+          background: '#d900c0',
+          '&:after': {
+            background: '#40ffce'
+          }
+        },
+        '&:last-of-type': {
+          background: '#ff5f2d',
+          '&:after': {
+            background: '#9058ff'
+          }
+        }
+      },
+      '@keyframes o-rotate-360': {
+        '0%': {
+          transform: 'rotate(0)'
+        },
+        '100%': {
+          transform: 'rotate(360deg)'
+        }
+      }
     },
     '& .modal-body': {
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      ...(sm && {
-        flexDirection: 'column'
+      position: 'relative',
+      background: 'white',
+      border: '2px solid transparent',
+      /* background-origin: border-box, */
+      backgroundClip: 'padding-box, border-box',
+      ...(md && {
+        height: '100vh'
       }),
-      '& .select-item': {
+      '& .modal-title': {
+        fontSize: '1.7rem',
+        fontWeight: 600,
+        textAlign: 'center',
+        padding: '40px 50px'
+      },
+      '& .modal-content': {
         display: 'flex',
-        flexDirection: 'column',
+        alignItems: 'flex-start',
         justifyContent: 'center',
-        alignItems: 'center',
-        padding: 10,
-        textDecoration: 'none',
-        transition: 'all 0.3s',
-        '& img': {
-          height: 400,
+        padding: '0px 30px 30px 40px',
+        position: 'relative',
+        ...(sm && {
+          flexDirection: 'column'
+        }),
+        '& .select-item': {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 10,
+          textDecoration: 'none',
           transition: 'all 0.3s',
-        },
-        '&:hover img': {
-          transition: 'all 0.3s',
-          transform: 'scale(1.1)'
-        },
-        '& button': {
-          marginTop: 30,
-          width: 200,
-          clipPath: 'polygon( calc(0px + 15px) 0, calc(100% - 15px) 0, 100% 50%, calc(100% - 15px) 100%, calc(0px + 15px) 100%, 0 50% )',
-          display: 'inline-block',
-          fontWeight: 700,
-          fontSize: 20,
-          color: '#fff',
-          lineHeight: 1,
-          padding: '14px 26px',
-          transition: 'all 0.3s',
-          position: 'relative',
-          background: '#5c49d0',
-          textTransform: 'capitalize',
-          ...((sm || md) && {
-            width: 160,
-            fontSize: 15,
-          }),
-          '&:hover': {
+          '& img': {
+            height: 400,
+            transition: 'all 0.3s',
+          },
+          '&:hover img': {
+            transition: 'all 0.3s',
+            // transform: 'scale(1.1)'
+          },
+          '& button': {
+            marginTop: 30,
+            width: 200,
+            clipPath: 'polygon( calc(0px + 15px) 0, calc(100% - 15px) 0, 100% 50%, calc(100% - 15px) 100%, calc(0px + 15px) 100%, 0 50% )',
+            display: 'inline-block',
+            fontWeight: 700,
+            fontSize: 20,
+            color: '#fff',
+            lineHeight: 1,
+            padding: '14px 26px',
+            transition: 'all 0.3s',
+            position: 'relative',
+            background: '#5c49d0',
+            textTransform: 'capitalize',
+            ...((sm || md) && {
+              width: 160,
+              fontSize: 15,
+            }),
+            '&:hover': {
+              background: '#cf3464',
+            }
+          },
+          '&:hover button': {
             background: '#cf3464',
+          },
+        },
+        '& .separater': {
+          padding: '0 25px',
+          ...(sm && {
+            display: 'none'
+          }),
+          '& .vertical-line': {
+            backgroundColor: '#DCE2E6',
+            width: 1,
+            height: '90%',
+            position: 'absolute',
+            left: '50%',
+            transform: 'translateX(-50%)',
+          },
+          '& .mark': {
+            backgroundColor: '#DCE2E6',
+            padding: '10px',
+            borderRadius: '50%',
+            fontSize: '24px',
+            color: 'white',
+            textAlign: 'center',
+            position: 'relative',
+            marginTop: '180px',
+            width: 50,
+            height: 50,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }
-        },
-        '&:hover button': {
-          background: '#cf3464',
-        },
+        }
+      },
+      '& .modal-actions button': {
+        display: 'none',
+        ...((sm || md) && {
+          display: 'block',
+        })
       }
     },
-    '& .modal-actions button': {
-      display: 'none',
-      ...((sm || md) && {
-        display: 'block',
-        position: 'fixed',
-        bottom: 10
-      })
-    }
   })
 })
